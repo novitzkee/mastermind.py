4fa018ad45f99025e3b737dd199c7cadf2b4f71a
Kacper Sieniawski <kacper.sieniawski@woodward.com>
2023-02-21 16:03:41 +0100

    Add Game class

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from random import randint
 
 
+class Game:
+    def __init__(self, is_difficult: bool, digits_limit: int = 5, trials_limit: int = 12):
+        self.__trials_limit: int = trials_limit
+        self.__code: str = Generator(digits_limit, is_difficult).generate_code()
+
+    def check_try(self, tried_code: str) -> str:
+        verdict: str = ""
+        tmp_code: str = self.__code
+        indexes: list[int] = [i for i in range(len(tmp_code))]
+        zipped_codes: tuple = tuple(zip(indexes, tmp_code, tried_code))
+
+        for i, code_chr, tried_code_chr in zipped_codes:
+            if code_chr == tried_code_chr:
+                verdict += '|'
+                tmp_code = tmp_code[:i] + '!' + tmp_code[i+1:]
+            elif tried_code_chr in self.__code:
+                verdict += "'"
+            else:
+                verdict += ' '
+
+        return verdict
+
+
 class Generator:
     def __init__(self, digits_limit: int, is_difficult: bool):
         self.__digits_limit: int = digits_limit
