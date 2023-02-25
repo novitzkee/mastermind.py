1ca523f86aa9fd7781e4f171524b6d8efb76c6ae
the-metapod <novitzkimain@gmail.com>
2023-02-25 19:35:02 +0100

    added basic parsers and operators

--- parsing.py
+++ parsing.py
@@ -1,8 +1,9 @@
 from enum import Enum
-from typing import Callable, Optional, TypeVar
+from functools import partial
+from typing import Any, Callable, Iterable, Optional, TypeVar
 
-T = TypeVar("T")
-R = TypeVar("R")
+T = TypeVar("T", contravariant=True)
+R = TypeVar("R", covariant=True)
 
 Parser = Callable[[T], Optional[R]]
 Predicate = Callable[[T], bool]
@@ -14,40 +15,77 @@ class YesOrNo(Enum):
     NO = 'n'
 
 
-# Common parsers
+def parse_y_or_n(input: str) -> Optional[YesOrNo]:
+    return {
+        'y': YesOrNo.YES,
+        'n': YesOrNo.NO
+    }.get(input)
 
 
 def parse_yes_or_no(input: str) -> Optional[YesOrNo]:
     return {
-        'y': YesOrNo.YES,
-        'n': YesOrNo.NO
-    }[input]
+        'yes': YesOrNo.YES,
+        'no': YesOrNo.NO
+    }.get(input)
 
 
-def parse_int(input: str) -> Optional[int]:
+def parse_int(radix: int, input: str) -> Optional[int]:
     try:
-        return int(input)
+        return int(input, radix)
     except TypeError:
+        return None
+    except ValueError:
         return None
 
 
-def parse_string(input: str) -> Optional[str]:
+def parse_string(input: str) -> str:
     return input
 
 
 # Parser operators
 
 
-def such_that(parser: Parser[T, R], predicate: Predicate[R]) -> Parser[T, R]:
-    return lambda input: satisfying(parser(input), predicate)
+def such_that(parser: Parser[T, R], *predicates: Predicate[R]) -> Parser[T, R]:
+    return lambda input: satisfying_all(parser(input), *predicates)
 
 
 def sanitized(parser: Parser[T, R], sanitizer: Function[T, T]) -> Parser[T, R]:
     return lambda input: parser(sanitizer(input))
 
 
-# Util function that should not be part of API and is here only because of lacking Python features
+def any_of(*parsers: Parser[T, R]) -> Parser[T, R]:
+    return lambda input: next(filter(lambda result: result is not None, [parser(input) for parser in parsers]), None)
 
 
-def satisfying(maybeT: Optional[T], predicate: Predicate[T]) -> Optional[T]:
-    return maybeT if maybeT is not None and predicate(maybeT) else None
+# Util functions
+
+
+def satisfying_all(optional: Optional[T], *predicates: Predicate[T]) -> Optional[T]:
+    if optional is None:
+        return None
+    else:
+        return optional if all(map(lambda predicate: predicate(optional), predicates)) else None
+
+
+# Parsers
+
+
+YES_OR_NO_PARSER = sanitized(any_of(parse_y_or_n, parse_yes_or_no), lambda s: str(s.lower()))
+
+
+MULTIFORMAT_INT_PARSER = any_of(partial(parse_int, 10), partial(parse_int, 0b10), partial(parse_int, 0x10))
+
+
+# Validations
+
+
+def is_contained_by(iterable: Iterable[T]) -> Predicate[T]:
+    return lambda input: input in iterable
+
+
+def has_length(length: int) -> Predicate[Iterable[Any]]:
+    return lambda iterable: len(list(iterable)) == length
+
+
+def all_satisfy(predicate: Predicate[T]) -> Predicate[Iterable[T]]:
+    return lambda iterable: all(map(lambda element: predicate(element), iterable))
